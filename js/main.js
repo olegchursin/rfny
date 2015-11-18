d17987a43b5265ccb8e4169120c1917fc625122e
olegchursin <olegchursin@gmail.com>
2015-11-18 12:09:39 -0500

    add site pages with content, styles and some jQuery methods

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  // Toggle contact form on Sponsors page
+
+  $("#sponsors-contact-toggle").click(function(){
+    $("#sponsors-contact-form").toggle();
+  });
+
+});
